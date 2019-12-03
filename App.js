5c36a908ccd7b08e13f8a610ac2ced2be6fd511e
deepblue28 <ryan.simms@hotmail.com>
2019-12-02 22:16:29 -0500

    all is well

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import * as firebase from 'firebase';
+
+// Web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDm0r2EGdOnn18spYvMTowdIitryv--sIA",
+  authDomain: "to-do-list-7e0cf.firebaseapp.com",
+  databaseURL: "https://to-do-list-7e0cf.firebaseio.com",
+  projectId: "to-do-list-7e0cf",
+  storageBucket: "to-do-list-7e0cf.appspot.com",
+  messagingSenderId: "426524082004",
+  appId: "1:426524082004:web:319924fc065494cbee12c1",
+  measurementId: "G-MS2562GBPD"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+export default class App extends React.Component {
+  render() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
     </View>
   );
+  }
 }
 
 const styles = StyleSheet.create({
