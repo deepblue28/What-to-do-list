79afe68f5fcae2a06b3a4233662a60c2dd772a10
deepblue28 <ryan.simms@hotmail.com>
2019-12-02 23:34:00 -0500

    almost there!

--- App.js
+++ App.js
@@ -21,16 +21,75 @@ firebase.initializeApp(firebaseConfig);
 
 
 export default class App extends React.Component {
+
+  constructor(props){
+    super(props)
+
+    this.state = ({
+      email:'',
+      password:''
+    })
+  }
+
+  signUpUser = (email,password) =>{
+    try {
+      if(this.state.length<8)
+      {
+        alert("Password can't have less than 8 characters!")
+        return;
+      }
+      firebase.auth().createUserWithEmailAndPassword(email, password)
+    }
+    catch(error){
+      console.log(error.topString())
+    }
+
+  }
+
+  LoginUser = (email,password) =>{
+
+  }
+
   render() {
   return (
     <Container style={styles.container}>
       <Form>
-        <Item>
+        <Item floatingLabel>
           <Label>Email</Label>
-          <Input autoCorrect={false}
-                 autoCapitalize="none"
+          <Input 
+          autoCorrect={false}
+          autoCapitalize="none"
+          onChangeText={(email) => this.setState({ email })} 
           />
         </Item>
+
+        <Item floatingLabel>
+          <Label>Password</Label>
+          <Input 
+          secureTextEntry={true}
+          autoCorrect={false}
+          autoCapitalize="none"
+          onChangeText={(password) => this.state.email({ password })}
+          />
+        </Item>
+
+        <Button style={{ marginTop: 12 }}
+          full
+          rounded
+          warning
+          onPress={()=> this.LoginUser(this.state.email,this.state.password)}
+        >
+          <Text style={{ color: 'black' }}>Login</Text>
+        </Button>
+
+        <Button style={{ marginTop: 12 }}
+          full
+          rounded
+          primary
+          onPress={()=> this.SignUpUser(this.state.email,this.state.password)}
+        >
+          <Text style={{ color: 'white' }}>Sign Up</Text>
+        </Button>
 
       </Form>
     </Container>
@@ -43,5 +102,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     justifyContent: 'center',
+    padding: 12,
   },
 });
